2d75b5a34f79545d06e116c242bed31f29ed6f1b
Janchii <Jani.Ylimartimo@gmail.com>
2020-09-09 06:29:50 +0300

    ex6 ready

---
+++ model/conn.php
@@ -0,0 +1,15 @@
+<?php
+	try
+	{
+	 $dsn = "mysql:host=localhost;dbname=io_exb";
+	 $db = new PDO ($dsn, "root", "jani1232");
+
+   //print ("Connected\n");
+	}
+	catch (PDOException $e)
+	{
+	 print ("Cannot connect to server\n");
+	 print ("Error code: " . $e->getCode () . "\n");
+	 print ("Error message: " . $e->getMessage () . "\n");
+	}
+?>
